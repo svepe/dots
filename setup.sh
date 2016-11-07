5b640128f37fb7f3248c9b7567fe05908037a50f
Svetlin Penkov <sv.penkov@gmail.com>
2016-11-07 09:18:48 +0000

    Add spacemacs config

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #/bin/sh	
 
+# TODO:
+# * Add dots path instead of hardcoding ~/Scripts/dots
+# * Add neovim installation
+# * Add font installation
+
 # promptline
 ln -s ~/Scripts/dots/promptline.sh   ~/.promptline.sh 
 
@@ -22,8 +27,20 @@ if [ ! -d "~/.config/nvim/bundle" ]; then
   mkdir ~/.config/nvim/bundle
 fi
 
-# spacemacs
+# Install emacs
+if [ ! -f /usr/local/bin/emacs ]; then
+  wget http://ftp.gnu.org/gnu/emacs/emacs-24.5.tar.xz /tmp/emacs-24.5.tar.xz
+  tar -xf /tmp/emacs-24.5.tar.* --directory /tmp/
+  cd /tmp/emacs-24.5
+  ./configure
+  make -j4
+  sudo make install
+fi
+
+# Install spacemacs
+git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 ln -s ~/Scripts/dots/spacemacs       ~/.spacemacs
+sudo ln -s /usr/local/bin/emacs /usr/local/bin/spacemacs
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.config/nvim/bundle/Vundle.vim
 
