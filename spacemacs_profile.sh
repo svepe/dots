0aab9d257b6d04e66265654057747d97db47330c
Svetlin Penkov <sv.penkov@gmail.com>
2017-09-06 10:42:42 +0100

    Add the smv command

--- spacemacs_profile.sh
+++ spacemacs_profile.sh
@@ -1,4 +1,3 @@
 export ALTERNATE_EDITOR=""
-export EDITOR="emacsclient -t"                  # $EDITOR should open in terminal
-export VISUAL="emacsclient -c -a emacs"         # $VISUAL opens in GUI with non-daemon as alternate
-
+export EDITOR="emacsclient -t -s term"          # $EDITOR should open in terminal
+export VISUAL="emacsclient -c -s gui -a emacs"  # $VISUAL opens in GUI with non-daemon as alternate
